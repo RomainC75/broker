10b99950e6de4acc6d9c38983bf322fd293ca006
Romain Chenard <romain.chenard@saegus.com>
2024-08-03 10:36:08 +0200

    front shows content

--- front/src/context/socket.context.tsx
+++ front/src/context/socket.context.tsx
@@ -11,31 +11,33 @@ const BROKER_HOST = process.env.BROKER_HOST_FROM_FRONT || "";
 const BROKER_PORT = process.env.BROKER_PORT || "";
 
 const WS_URL = `ws://${BROKER_HOST}:${BROKER_PORT}/reader`;
-// const socket = new WebSocket(WS_URL)
+const socket = new WebSocket(WS_URL)
 
 function SocketProviderWrapper(props: PropsWithChildren) {
   const [myState, setMyState] = useState<number>(3);
-  // const [data, setData] = useState<TopicMapDto | null>()
-  const [data, setData] = useState<TopicMapDto | null>(JSON.parse(dataJson))
+  const [data, setData] = useState<TopicMapDto | null>()
+  // const [data, setData] = useState<TopicMapDto | null>(JSON.parse(dataJson))
   
   
 
   useEffect(() => {
-    // socket.onopen = () => {
-    //   // setMessage('Connected')
-    //   console.log("- CONNECTED")
-    // };
+    socket.onopen = () => {
+      // setMessage('Connected')
+      console.log("- CONNECTED")
+    };
 
-    // socket.onmessage = (e) => {
-    //   // setMessage("Get message from server: " + e.data)
-    //   console.log("->", e.data)
-    //   console.log("==>", JSON.parse(e.data))
+    socket.onmessage = (e) => {
+      // setMessage("Get message from server: " + e.data)
+      console.log("->", e.data)
+      const content = JSON.parse(e.data)
+      console.log("==>", content)
+      setData(content)
 
-    // };
+    };
 
-    // socket.onclose = () => {
-    //   console.log("closed ")
-    // };
+    socket.onclose = () => {
+      console.log("closed ")
+    };
 
     console.log("- ", data)
 
