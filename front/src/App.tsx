5a0077383abe4aaf7d57b484921c08a1e53854a3
Romain Chenard <romain.chenard@saegus.com>
2024-08-09 21:01:54 +0200

    front sso + remove watcher/client int backend

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,9 +3,17 @@ import './App.css'
 import { ISocketContext } from './@types/socketContext.type'
 import { SocketContext } from './context/socket.context'
 import HomePage from './pages/HomePage'
+import { useMsal, useMsalAuthentication } from '@azure/msal-react'
+import { InteractionType } from '@azure/msal-browser'
 
 function App() {
   console.log("-> ", process.env.BROKER_HOST)
+  // const { user, handleUser } = useContext(AuthContext) as IAuthContext;
+  useMsalAuthentication(InteractionType.Redirect, {
+    scopes:["User.Read"]
+  })
+  const {instance} = useMsal()
+  console.log("get active : ", instance.getActiveAccount())
   const { myState } = useContext(SocketContext) as ISocketContext;
   return (
     <>
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { SocketProviderWrapper } from './context/socket.context.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { SocketProviderWrapper } from "./context/socket.context.tsx";
+import AuthProvider from "./auth/Auth.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
-  <>
-    <SocketProviderWrapper>
-    <App />
-
-    </SocketProviderWrapper>
-  </>
+    <AuthProvider>
+      <SocketProviderWrapper>
+        <App />
+        {/* <div>HHHHHHEEEE</div> */}
+      </SocketProviderWrapper>
+    </AuthProvider>
   // </React.StrictMode>,
-  
-)
+);
